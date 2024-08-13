19e3c900698e3e31cd1a9997a9e7edd1245ca610
Felix Zhang <felix.zhang@coinbase.com>
2024-08-13 12:29:35 -0700

    scaffold

--- packages/client/jest.config.ts
+++ packages/client/jest.config.ts
@@ -57,5 +57,5 @@ export default {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
